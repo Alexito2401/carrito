b70adc65e604da4ba3c4ecbe3ea42eaaa1b4a755
Alex Bernal <albetr@floridauniversitaria.es>
2021-12-10 09:36:09 +0100

    feat(#2):Conxeion con api json-server

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,15 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-
 export class HomePage {
-
-  constructor() { }
-  public productos: Producto[] = [
+  constructor(
+  private menu: MenuController,  
+  private http: HttpClient) {}
+  public categorias = Array<Categoria>();
+  public productos = Array<Producto>();
+  /*public productos: Producto[] = [
     //Ordenadores
     new Producto(1, "Acer Predator Orion 3000", "1499,00", "Samsung", "../../assets/imgs/productos/ordenador.png", 1),
     new Producto(2, "MSI MAG Codex 5", "1499,00", "MSI", "../../assets/imgs/productos/ordenador2.png", 1),
@@ -29,49 +33,87 @@ export class HomePage {
     new Producto(12, "Logitech G502 Hero", "49,99", "Logitech", "../../assets/imgs/productos/periferico4.png", 3),
 
     //Impresoras
-    new Producto(9, 'HP Officejet Pro 6230', "83,87", "HP", "../../assets/imgs/productos/impresora.png", 4),
-    new Producto(10, "Epson Workforce WF-2010W", "86,98", "Epson", "../../assets/imgs/productos/impresora2.png", 4),
-    new Producto(11, "HP Officejet 200", "252,83", "HP", "../../assets/imgs/productos/impresora3.png", 4),
-    new Producto(12, "Zebra GK420d", "439,70", "Zebra", "../../assets/imgs/productos/impresora4.png", 4),
+    new Producto(13, 'HP Officejet Pro 6230', "83,87", "HP", "../../assets/imgs/productos/impresora.png", 4),
+    new Producto(14, "Epson Workforce WF-2010W", "86,98", "Epson", "../../assets/imgs/productos/impresora2.png", 4),
+    new Producto(15, "HP Officejet 200", "252,83", "HP", "../../assets/imgs/productos/impresora3.png", 4),
+    new Producto(16, "Zebra GK420d", "439,70", "Zebra", "../../assets/imgs/productos/impresora4.png", 4),
 
     //Televisores
-    new Producto(13, 'Xiaomi Mi TV P1 43"', "339", "Xiaomi", "../../assets/imgs/productos/tele.png", 5),
-    new Producto(14, 'Samsung UE55AU7172 55" ', "489", "Samsung", "../../assets/imgs/productos/tele2.png", 5),
-    new Producto(15, 'Toshiba 32LA2063DG 32"', "199,99", "Toshiba", "../../assets/imgs/productos/tele3.png", 5),
-    new Producto(16, 'TD Systems K55DLG12US 55"', "398,50", "Td systems", "../../assets/imgs/productos/tele4.png", 5),
-  ];
+    new Producto(17, 'Xiaomi Mi TV P1 43"', "339", "Xiaomi", "../../assets/imgs/productos/tele.png", 5),
+    new Producto(18, 'Samsung UE55AU7172 55" ', "489", "Samsung", "../../assets/imgs/productos/tele2.png", 5),
+    new Producto(19, 'Toshiba 32LA2063DG 32"', "199,99", "Toshiba", "../../assets/imgs/productos/tele3.png", 5),
+    new Producto(20, 'TD Systems K55DLG12US 55"', "398,50", "Td systems", "../../assets/imgs/productos/tele4.png", 5),
+  ];*/
 
   public icono_carrito: boolean = false;
-
-  public categorias: Categoria[] = [
-    new Categoria(1, "Ordenadores"),
-    new Categoria(2, "Portatiles"),
-    new Categoria(3, "Perifericos"),
-    new Categoria(4, "Impresoras"),
-    new Categoria(5, "Televisores"),
-  ];
+  private url: string = 'http://localhost:3000/';
 
   public carrito: Carrito = new Carrito();
   public visible: boolean = true;
 
   changeVisible(): void {
-    this.visible = !this.visible
+    this.visible = !this.visible;
   }
 
   changeIcon(): void {
-    this.carrito.productos.length != 0 ? this.icono_carrito = true : this.icono_carrito = false;
+    this.carrito.productos.length != 0
+      ? (this.icono_carrito = true)
+      : (this.icono_carrito = false);
   }
 
+  
+  openFirst() {
+    this.menu.enable(true, 'first');
+    this.menu.open('first');
+  }
 
+  openEnd() {
+    this.menu.open('end');
+  }
+
+  openCustom() {
+    this.menu.enable(true, 'custom');
+    this.menu.open('custom');
+  }
 
   ngOnInit(): void {
+    this.http
+      .get(this.url + 'Categorias')
+      .toPromise()
+      .then((e) =>
+        Object.values(e).map((f) =>
+          this.categorias.push(new Categoria(f.id, f.nombre))
+        )
+      )
+      .then((g) =>
+        this.categorias.forEach((h) =>
+          this.http
+            .get(this.url + h.nombre)
+            .toPromise()
+            .then((e) =>
+              Object.values(e).map(
+                (f) =>
+                  this.productos.push(
+                    new Producto(
+                      f.id,
+                      f.nombre,
+                      f.precio,
+                      f.marca,
+                      f.imagen,
+                      f.categoria
+                    )
+                  ),
+                console.log(this.productos)
+              )
+            )
+        )
+      )
+      .catch((err) => console.log('error'));
   }
-
 }
 
 class Producto {
-
-  public id: number;
+  public id: number | string;
   public nombre: string;
   public precio: string;
   public marca: string;
@@ -79,23 +121,29 @@ class Producto {
   public idCategoria: number;
   public cantidad: number;
 
-  constructor(id: number, nombre: string, precio: string, marca: string, link: string = "", categoria: number) {
+  constructor(
+    id: number | string,
+    nombre: string,
+    precio: string,
+    marca: string,
+    link: string = '',
+    categoria: number
+  ) {
     this.id = id;
     this.nombre = nombre;
     this.precio = precio;
     this.marca = marca;
     this.link = link;
-    this.idCategoria = categoria
+    this.idCategoria = categoria;
     this.cantidad = 1;
   }
 }
 
 class Categoria {
-
-  public id: number;
+  public id: number | string;
   public nombre: string;
 
-  constructor(id: number, nombre: string) {
+  constructor(id: number | string, nombre: string) {
     this.id = id;
     this.nombre = nombre;
   }
@@ -109,21 +157,23 @@ class Carrito {
   }
 
   agregarCarrito = (producto: Producto) => {
-    let pro = this.productos.findIndex((e) => e.id == producto.id);
-    pro == -1 ? this.productos.push(producto) : this.productos[pro].cantidad += 1;
-  }
+    const pro = this.productos.findIndex((e) => e.id == producto.id);
+    pro == -1
+      ? this.productos.push(producto)
+      : (this.productos[pro].cantidad += 1);
+  };
 
-  borrarProducto = (id: number) => {
+  borrarProducto = (id: number | string) => {
     let pro = this.productos.findIndex((e) => e.id == id);
     this.productos[pro].cantidad -= 1;
 
     if (this.productos[pro].cantidad == 0) {
       this.productos.splice(pro, 1);
     }
-  }
+  };
 
-  borrarTotal = (id: number) => {
+  borrarTotal = (id: number | string) => {
     let pro = this.productos.findIndex((e) => e.id == id);
     this.productos.splice(pro, 1);
-  }
+  };
 }
